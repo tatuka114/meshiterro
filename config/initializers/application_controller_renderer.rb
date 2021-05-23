b682af5a5c90ede5cd3f1b6914bae202f514a567
tatuka114 <ec2-user@ip-172-31-75-135.ec2.internal>
2021-05-23 10:55:20 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '58b5b3f9101a50d422b52172fc2cebc9a344155f240ce99eed53cb6ed8c970b7131ef96a7131f2e266c97f1841ee803c991a15c05968f38311337ef87644cf61'
